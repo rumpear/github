84bb957bc208238f7d1f020e4c8956b1f7200d2e
rumpear <roman.f.grusha@gmail.com>
2022-10-28 12:29:11 +0300

    API refactoring

--- src/services/githubApi.tsx
+++ src/services/githubApi.tsx
@@ -2,8 +2,7 @@
 import { Octokit } from 'octokit';
 import { IData, IResponse } from './types';
 
-const TOKEN =
-  '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';
+const TOKEN = '';
 
 const octokit = new Octokit({
   auth: TOKEN,
@@ -18,12 +17,6 @@ export const getUsersData: TGetUsersData = async (query, page) => {
     // per_page: 15,
     page,
   });
-  //   const { data }: IData = await octokit.request('GET /search/users', {
-  //     q: query,
-  //     // sort: 'repositories',
-  //     // per_page: 15,
-  //     page: 1,
-  //   });
 
   return data;
 };
