77d0f190938c6bc2ca214cdef90600521b77f3c5
rumpear <roman.f.grusha@gmail.com>
2022-10-28 12:28:25 +0300

    useFetchUsers refactoring

--- src/hooks/useFetchUsers.ts
+++ src/hooks/useFetchUsers.ts
@@ -1,24 +1,31 @@
 import { useCallback, useEffect, useState } from 'react';
 import { getUsersData } from '../services/githubApi';
-import { IResponse } from '../services/types';
+import { IItems } from '../services/types';
+import { usePagination } from './';
 
 const MINIMAL_QUERY_LENGTH = 3;
+const PER_PAGE = 30;
 
-type TUseFetchUsers = (
-  query: string
-  //   page: number
-) => {
-  users: IResponse | null;
+type TUseFetchUsers = (query: string) => {
+  usersData: IItems[];
   loading: boolean;
   error: string;
-  setPage: React.Dispatch<React.SetStateAction<number>>;
+  page: number;
+  totalPages: number;
+  nextPage: () => void;
+  goToPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export const useFetchUsers: TUseFetchUsers = (query = '') => {
-  const [users, setUsers] = useState<IResponse | null>(null);
+export const useFetchUsers: TUseFetchUsers = (query) => {
+  const [usersData, setUsersData] = useState<IItems[]>([]);
+  const [totalUsersCount, setTotalUsersCount] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const [page, setPage] = useState(1);
+
+  const { page, totalPages, nextPage, goToPage } = usePagination(
+    PER_PAGE,
+    totalUsersCount
+  );
 
   const fetchData = useCallback(async () => {
     const isQueryLessThanReqLength = query.length < MINIMAL_QUERY_LENGTH;
@@ -30,7 +37,15 @@ export const useFetchUsers: TUseFetchUsers = (query = '') => {
     setLoading(true);
     try {
       const data = await getUsersData(query, page);
-      setUsers(data);
+
+      setUsersData((prev) => {
+        if (data.items.length) {
+          return [...prev, ...data.items];
+        }
+        return prev;
+      });
+
+      setTotalUsersCount(data.total_count);
     } catch (error) {
       const e = error as Error;
       setError(e.message);
@@ -39,12 +54,12 @@ export const useFetchUsers: TUseFetchUsers = (query = '') => {
   }, [query, page]);
 
   useEffect(() => {
-    setPage(1);
-  }, [query]);
-
-  useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  return { users, loading, error, setPage };
+  useEffect(() => {
+    setUsersData([]);
+  }, [query]);
+
+  return { usersData, loading, error, nextPage, goToPage, page, totalPages };
 };
