64f4b25a257ad6ee69b0f8f8abd9c6a6042473b2
rumpear <roman.f.grusha@gmail.com>
2022-11-02 15:39:33 +0200

    localStorageData added

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { InView } from 'react-intersection-observer';
+import { useLocalStorage } from '../../hooks';
 import { IFullUser } from '../../services/types';
 import { UserCard } from '../UserCard';
 import './CardsList.style.scss';
@@ -20,6 +21,10 @@ const CardsList = ({
   loading,
 }: ICardsListProps) => {
   const [currentUser, setCurrentUser] = useState<IFullUser | null>(null);
+  const { localStorageData, setLocalStorageData } = useLocalStorage<
+    IFullUser[]
+  >('favoriteUsers', []);
+
   const showNextPage = page < totalPages;
 
   const handleCardClick = (currentUserLogin: string) => {
@@ -38,35 +43,90 @@ const CardsList = ({
     }
   };
 
+  const addToFavorites = (currentUserLogin: string) => {
+    const user = users.filter((user) => user.login === currentUserLogin);
+    // console.log(user, 'user');
+    // console.log(localStorageData, 'localStorageData');
+
+    // const usersLogins = localStorageData.map((user) => user.login);
+    // console.log(usersLogins, 'usersLogins');
+    // const uniqueUsersData = localStorageData.filter((user) => {
+    //   console.log(user.login, 'user.login');
+    //   console.log(usersLogins.includes(user.login));
+    //   return !usersLogins.includes(user.login);
+    // });
+
+    // console.log(uniqueUsersData, 'uniqueUsersData');
+
+    // console.log(
+    //   !localStorageData.length && !!user,
+    //   '!!localStorageData.length && !!user'
+    // );
+    // console.log(!!user, '!!user');
+    // console.log(!localStorageData.length, '!!localStorageData.length');
+    // console.log(localStorageData, 'localStorageData addToFavorites');
+
+    if (!localStorageData.length && !!user) {
+      return setLocalStorageData(user);
+    }
+
+    if (user) {
+      setLocalStorageData((prev) => {
+        console.log(prev, 'prev');
+        const uniqueUsersData = prev.filter((user) => {
+          return user.login !== currentUserLogin;
+        });
+        console.log(uniqueUsersData, 'uniqueUsersData');
+
+        // const usersLogins = prev.map((user) => user.login);
+
+        // console.log(usersLogins, 'usersLogins');
+        // const uniqueUsersData = user.filter((user) => {
+        //   console.log(user.login, 'user.login');
+        //   console.log(usersLogins.includes(user.login));
+        //   return !usersLogins.includes(user.login);
+        // });
+        // console.log(uniqueUsersData, 'uniqueUsersData');
+        return [...prev, ...uniqueUsersData];
+      });
+    }
+  };
+
+  console.log(localStorageData, 'localStorageData');
+
   return (
     <>
       <div className='Card-wrapper'>
         {users.map((user: IFullUser) => {
           return (
-            <div
-              key={user.login}
-              className='Card'
-              onClick={() => handleCardClick(user.login)}
-            >
-              <div className='Card-thumb'>
-                <img
-                  className='Card-avatar'
-                  src={user.avatar_url}
-                  alt={user.login}
-                />
+            <div key={user.login} className='Card'>
+              <div onClick={() => handleCardClick(user.login)}>
+                <div className='Card-thumb'>
+                  <img
+                    className='Card-avatar'
+                    src={user.avatar_url}
+                    alt={user.login}
+                  />
+                </div>
+                <div className='Card-textWrapper'>
+                  <p className='Card-login'>{user.login}</p>
+                  <p className='Card-bio'>{user.bio}</p>
+                </div>
               </div>
-              <div className='Card-text-wrapper'>
-                <p className='Card-login'>{user.login}</p>
-                <p className='Card-bio'>{user.bio}</p>
-              </div>
-              {/* <button type='button'>Fav</button> */}
+              <button
+                type='button'
+                className='Card-favBtn'
+                onClickCapture={() => addToFavorites(user.login)}
+              >
+                Fav
+              </button>
             </div>
           );
         })}
         {showNextPage && (
           <InView
             as='div'
-            className='Card-next-page'
+            className='Card-nextPageBtn'
             onChange={handlePageChange}
           />
         )}
