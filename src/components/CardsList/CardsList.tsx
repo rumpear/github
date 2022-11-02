e6480dc7c4087a00f13e21d03e22643e10d84a94
Roman <roman.f.grusha@gmail.com>
2022-11-02 02:04:02 +0200

    CardList refactoring, Intersection Observer added

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,33 +1,78 @@
+import { useState } from 'react';
+import { InView } from 'react-intersection-observer';
 import { IFullUser } from '../../services/types';
+import { UserCard } from '../UserCard';
 import './CardsList.style.scss';
 
 interface ICardsListProps {
   users: IFullUser[];
+  nextPage: () => void;
+  totalPages: number;
+  page: number;
+  loading: boolean;
 }
 
-const CardsList = ({ users }: ICardsListProps) => {
+const CardsList = ({
+  users,
+  nextPage,
+  page,
+  totalPages,
+  loading,
+}: ICardsListProps) => {
+  const [currentUser, setCurrentUser] = useState<IFullUser | null>(null);
+  const showNextPage = page < totalPages;
+
+  const handleCardClick = (currentUserLogin: string) => {
+    const user = users.find((user) => user.login === currentUserLogin);
+    const isUserExist = user ?? null;
+    setCurrentUser(isUserExist);
+  };
+
+  const resetUser = () => {
+    setCurrentUser(null);
+  };
+
+  const handlePageChange = (inView: boolean): void => {
+    if (inView && !loading) {
+      nextPage();
+    }
+  };
+
   return (
-    <div className='Card-wrapper'>
-      {/* : IFullUser */}
-      {users.map((user, idx) => {
-        return (
-          <div key={user.login + idx} className='Card'>
-            <div className='Card-thumb'>
-              <img
-                className='Card-avatar'
-                src={user.avatar_url}
-                alt={user.login}
-              />
+    <>
+      <div className='Card-wrapper'>
+        {users.map((user: IFullUser) => {
+          return (
+            <div
+              key={user.login}
+              className='Card'
+              onClick={() => handleCardClick(user.login)}
+            >
+              <div className='Card-thumb'>
+                <img
+                  className='Card-avatar'
+                  src={user.avatar_url}
+                  alt={user.login}
+                />
+              </div>
+              <div className='Card-text-wrapper'>
+                <p className='Card-login'>{user.login}</p>
+                <p className='Card-bio'>{user.bio}</p>
+              </div>
+              {/* <button type='button'>Fav</button> */}
             </div>
-            <div className='Card-text-wrapper'>
-              <p className='Card-login'>{user.login}</p>
-              <p className='Card-bio'>{user.bio}</p>
-            </div>
-            {/* <button type='button'>Fav</button> */}
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+        {showNextPage && (
+          <InView
+            as='div'
+            className='Card-next-page'
+            onChange={handlePageChange}
+          />
+        )}
+      </div>
+      {!!currentUser && <UserCard user={currentUser} resetUser={resetUser} />}
+    </>
   );
 };
 
