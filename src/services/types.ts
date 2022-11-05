9a0a9dd3b2474b404eb785d0866ccd3e11e9f790
Roman <77742141+rumpear@users.noreply.github.com>
2022-11-05 13:21:36 +0200

    types upd

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,3 +1,5 @@
+import { IFullUser } from '../interfaces';
+
 export interface IUsers {
   avatar_url: string;
   events_url: string;
@@ -31,42 +33,6 @@ export interface IUsersData {
 }
 
 export type TGetUsersData = (query: string, page: number) => Promise<IResponse>;
-
-export interface IFullUser {
-  avatar_url: string;
-  bio: string | null;
-  blog: string;
-  company: string | null;
-  created_at: string;
-  email: string | null;
-  events_url: string;
-  followers: number;
-  followers_url: string;
-  following: number;
-  following_url: string;
-  gists_url: string;
-  gravatar_id: string;
-  hireable: boolean;
-  html_url: string;
-  id: number;
-  location: string | null;
-  login: string;
-  name: string;
-  node_id: string;
-  organizations_url: string;
-  public_gists: number;
-  public_repos: number;
-  received_events_url: string;
-  repos_url: string;
-  site_admin: boolean;
-  starred_url: string;
-  subscriptions_url: string;
-  twitter_username: string | null;
-  type: string;
-  updated_at: string;
-  url: string;
-  isFavorite: boolean;
-}
 
 export interface IFullUserData {
   data: IFullUser;
