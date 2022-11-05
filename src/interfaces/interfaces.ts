3768b2c7b7b1511492da24dc02e4c23ea9502052
Roman <77742141+rumpear@users.noreply.github.com>
2022-11-05 13:25:30 +0200

    added global types

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,1 +1,57 @@
-export {};
+export interface IUsers {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string | null;
+  html_url: string;
+  id: number;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  score: number;
+  site_admin: boolean;
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  url: string;
+}
+
+export interface IFullUser {
+  avatar_url: string;
+  bio: string | null;
+  blog: string;
+  company: string | null;
+  created_at: string;
+  email: string | null;
+  events_url: string;
+  followers: number;
+  followers_url: string;
+  following: number;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string;
+  hireable: boolean;
+  html_url: string;
+  id: number;
+  location: string | null;
+  login: string;
+  name: string;
+  node_id: string;
+  organizations_url: string;
+  public_gists: number;
+  public_repos: number;
+  received_events_url: string;
+  repos_url: string;
+  site_admin: boolean;
+  starred_url: string;
+  subscriptions_url: string;
+  twitter_username: string | null;
+  type: string;
+  updated_at: string;
+  url: string;
+  isFavorite: boolean;
+}
