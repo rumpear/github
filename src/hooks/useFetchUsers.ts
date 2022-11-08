4dbaab708cbc981b5563aab1ebe771e1ea9cc527
rumpear <roman.f.grusha@gmail.com>
2022-11-08 15:37:43 +0200

    useFetchUsers refactoring

--- src/hooks/useFetchUsers.ts
+++ src/hooks/useFetchUsers.ts
@@ -21,10 +21,10 @@ export type TUseFetchUsers = (query: string) => {
 
 export const useFetchUsers: TUseFetchUsers = (query) => {
   const [usersData, setUsersData] = useState<IFullUser[]>([]);
+  const [favUsers, setFavUsers] = useState<IFullUser[]>([]);
   const [totalUsersCount, setTotalUsersCount] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const [favUsers, setFavUsers] = useState<IFullUser[]>([]);
 
   const { page, totalPages, nextPage, goToPage } = usePagination(
     PER_PAGE,
@@ -88,20 +88,22 @@ export const useFetchUsers: TUseFetchUsers = (query) => {
     setFavUsers(favoritesUsers);
   }, [localStorageData]);
 
+  // useEffect(() => {
+  //   setUsersData([]);
+
+  //   if (!query.length) {
+  //     setTotalUsersCount(0);
+  //   }
+  // }, [query]);
+
   useEffect(() => {
     setUsersData([]);
+    goToPage(1);
 
     if (!query.length) {
       setTotalUsersCount(0);
     }
-  }, [query]);
-
-  // useEffect(() => {
-  //   if (!query.length) {
-  //     setTotalUsersCount(0);
-  //     setUsersData([]);
-  //   }
-  // }, [query]);
+  }, [goToPage, query]);
 
   return {
     usersData,
@@ -117,3 +119,136 @@ export const useFetchUsers: TUseFetchUsers = (query) => {
     favUsers,
   };
 };
+
+// import { useCallback, useEffect, useState, useMemo } from 'react';
+// import { getUniqueUsersData } from '../utils';
+// import { MINIMAL_QUERY_LENGTH, PER_PAGE } from '../constants';
+// import { getUsersData } from '../services/githubApi';
+// import { usePagination, useLocalStorage } from './';
+// import { IFullUser } from '../interfaces';
+
+// export type TUseFetchUsers = (query: string) => {
+//   usersData: IFullUser[];
+//   loading: boolean;
+//   error: string;
+//   page: number;
+//   totalPages: number;
+//   nextPage: () => void;
+//   goToPage: React.Dispatch<React.SetStateAction<number>>;
+//   setUsersData: React.Dispatch<React.SetStateAction<IFullUser[]>>;
+//   localStorageData: IFullUser[];
+//   setLocalStorageData: React.Dispatch<React.SetStateAction<IFullUser[]>>;
+//   favUsers: IFullUser[];
+// };
+
+// export const useFetchUsers: TUseFetchUsers = (query) => {
+//   const [usersData, setUsersData] = useState<IFullUser[]>([]);
+//   const [favUsers, setFavUsers] = useState<IFullUser[]>([]);
+//   const [totalUsersCount, setTotalUsersCount] = useState<number>(0);
+//   const [loading, setLoading] = useState<boolean>(false);
+//   const [error, setError] = useState<string>('');
+
+//   const [page, setPage] = useState(1);
+//   // setPage((prev) => (usersData.length >= totalUsersCount ? prev : prev + 1));
+
+//   // const { page, totalPages, nextPage, goToPage } = usePagination(
+//   //   PER_PAGE,
+//   //   totalUsersCount
+//   // );
+
+//   const { localStorageData, setLocalStorageData } = useLocalStorage<
+//     IFullUser[],
+//     []
+//   >('favoriteUsers', []);
+
+//   const localStorageLogins = useMemo(
+//     () => localStorageData.map((user: IFullUser) => user.login),
+//     [localStorageData]
+//   );
+
+//   const fetchData = useCallback(async () => {
+//     const isQueryLessThanReqLength = query.length < MINIMAL_QUERY_LENGTH;
+
+//     if (!query || isQueryLessThanReqLength) {
+//       return;
+//     }
+
+//     setLoading(true);
+//     try {
+//       const { data, totalCount } = await getUsersData(query, page);
+//       const isDataExist = !!data.length;
+
+//       setUsersData((prev: IFullUser[]) => {
+//         const uniqueUsersData = getUniqueUsersData(prev, data);
+
+//         const uniqueUsersDataWithFavorites = uniqueUsersData.map(
+//           (user: IFullUser) => {
+//             return {
+//               ...user,
+//               isFavorite: localStorageLogins.includes(user.login),
+//             };
+//           }
+//         );
+
+//         return isDataExist ? [...prev, ...uniqueUsersDataWithFavorites] : prev;
+//       });
+
+//       setTotalUsersCount(totalCount);
+//     } catch (error) {
+//       const e = error as Error;
+//       setError(e.message);
+//     }
+//     setLoading(false);
+//   }, [query, page]);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, [fetchData]);
+
+//   useEffect(() => {
+//     const favoritesUsers = localStorageData.filter((user: IFullUser) => {
+//       return user.isFavorite;
+//     });
+
+//     setFavUsers(favoritesUsers);
+//   }, [localStorageData]);
+
+//   // useEffect(() => {
+//   //   setUsersData([]);
+
+//   //   if (!query.length) {
+//   //     setTotalUsersCount(0);
+//   //   }
+//   // }, [query]);
+
+//   // useEffect(() => {
+//   //   setUsersData([]);
+//   //   goToPage(1);
+
+//   //   if (!query.length) {
+//   //     setTotalUsersCount(0);
+//   //   }
+//   // }, [goToPage, query]);
+
+//   useEffect(() => {
+//     setUsersData([]);
+
+//     if (!query.length) {
+//       setTotalUsersCount(0);
+//     }
+//   }, [query]);
+
+//   return {
+//     usersData,
+//     setUsersData,
+//     loading,
+//     error,
+//     nextPage,
+//     goToPage,
+//     page,
+//     totalPages,
+//     localStorageData,
+//     setLocalStorageData,
+//     favUsers,
+//   };
+// };
