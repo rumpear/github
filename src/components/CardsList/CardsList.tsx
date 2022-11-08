25e62c3771ca16247a0f1d068983e0ef14f11d96
rumpear <roman.f.grusha@gmail.com>
2022-11-08 15:30:36 +0200

    CardsList refactoring

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,15 +1,13 @@
-import { useEffect, useRef } from 'react';
 import { InView } from 'react-intersection-observer';
+import { FAVORITES_BTN_LABEL } from '../../constants';
 import { IFullUser } from '../../interfaces';
 import './CardsList.style.scss';
-
-const FAVORITES_BTN_LABEL = { delete: 'Del', add: 'Add' };
 
 interface ICardsListProps {
   users: IFullUser[];
   loading?: boolean;
   isShowNextPage: boolean;
-  toggleFavoriteUser: (currUserLogin: string, isFavorite: boolean) => void;
+  toggleFavoriteUser: (user: IFullUser) => void;
   showCurrentUser: (user: IFullUser) => void;
   goToNextPage: (inView: boolean) => void;
 }
@@ -22,60 +20,51 @@ const CardsList = ({
   showCurrentUser,
   goToNextPage,
 }: ICardsListProps) => {
-  const isUserDataExist = !!users.length;
-
-  // useRef
-  // useEffect
+  const isLoading = !!users.length && loading;
 
   return (
     <div className='CardList'>
-      {isUserDataExist ? (
-        users.map((user: IFullUser) => {
-          const favoritesBtnLabel = user.isFavorite
-            ? FAVORITES_BTN_LABEL.delete
-            : FAVORITES_BTN_LABEL.add;
+      {users.map((user: IFullUser) => {
+        const favoritesBtnLabel = user.isFavorite
+          ? FAVORITES_BTN_LABEL.delete
+          : FAVORITES_BTN_LABEL.add;
 
-          return (
-            <div key={user.login} className='Card'>
-              <div
-                onClick={() => showCurrentUser(user)}
-                className='Card-container'
-              >
-                <div className='Card-thumb'>
-                  <img
-                    className='Card-avatar'
-                    src={user.avatar_url}
-                    alt={user.login}
-                  />
-                </div>
-                <div className='Card-textWrapper'>
-                  <p className='Card-login'>{user.login}</p>
-                  <p className='Card-bio'>{user.bio}</p>
-                </div>
+        return (
+          <div key={user.login} className='Card'>
+            <div
+              onClick={() => showCurrentUser(user)}
+              className='Card-container'
+            >
+              <div className='Card-thumb'>
+                <img
+                  className='Card-avatar'
+                  src={user.avatar_url}
+                  alt={user.login}
+                />
               </div>
-              <div className='Card-favBtn-wrapper'>
-                <button
-                  type='button'
-                  className='Card-favBtn'
-                  onClick={() =>
-                    toggleFavoriteUser(user.login, user.isFavorite)
-                  }
-                >
-                  {favoritesBtnLabel}
-                </button>
+              <div className='Card-textWrapper'>
+                <p className='Card-login'>{user.login}</p>
+                <p className='Card-bio'>{user.bio}</p>
               </div>
             </div>
-          );
-        })
-      ) : (
-        <p className='CardList-warning'>Nothing there</p>
-      )}
+            <div className='Card-favBtn-wrapper'>
+              <button
+                type='button'
+                className='Card-favBtn'
+                onClick={() => toggleFavoriteUser(user)}
+              >
+                {favoritesBtnLabel}
+              </button>
+            </div>
+          </div>
+        );
+      })}
 
       {isShowNextPage && (
         <InView as='div' className='Card-nextPageBtn' onChange={goToNextPage} />
       )}
 
-      {loading && <p className='CardList-loading'>Loading...</p>}
+      {isLoading && <p className='CardList-loading'>Loading...</p>}
     </div>
   );
 };
