bfdbc8cc1c6adfc5932e012b8561e26e4db4a3c9
rumpear <roman.f.grusha@gmail.com>
2022-11-08 15:31:25 +0200

    constants upd

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,10 @@ export const TOKEN = '';
 export const PER_PAGE = 10;
 
 export const MINIMAL_QUERY_LENGTH = 3;
+
+export const SWITCH_BTN_LABELS = {
+  favorites: 'Switch to Favorites',
+  search: 'Switch to Search',
+};
+
+export const FAVORITES_BTN_LABEL = { delete: 'Del', add: 'Add' };
