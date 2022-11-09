3f20dee6387c5e22e34fdd08201a1613a69efea5
rumpear <roman.f.grusha@gmail.com>
2022-11-09 15:51:15 +0200

    useLocalStorage refactoring

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -8,28 +8,6 @@ type TUseLocalStorage = <T, D>(
   setLocalStorageData: Dispatch<SetStateAction<T>>;
 };
 
-// export const useLocalStorage: TUseLocalStorage = <T, D>(
-//   key: string,
-//   defaultValue?: D
-// ) => {
-//   const ifLocalDataExist = localStorage.getItem(key);
-
-//   // try catch JSON.parse
-//   const getData = () => {
-//     return ifLocalDataExist ? JSON.parse(ifLocalDataExist) : defaultValue;
-//   };
-
-//   // try catch JSON.parse
-//   const setData = () => {
-//     return localStorage.setItem(key, JSON.stringify(localStorageData));
-//   };
-
-//   const [localStorageData, setLocalStorageData] = useState<T>(getData());
-//   useEffect(setData, [key, localStorageData]);
-
-//   return { localStorageData, setLocalStorageData };
-// };
-
 export const useLocalStorage: TUseLocalStorage = <T, D>(
   key: string,
   defaultValue?: D
