e9c4df8bac5bf09ecbbb173d785560f039b7654f
rumpear <roman.f.grusha@gmail.com>
2022-11-18 13:35:31 +0200

    usePagination hook removed

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useObserver } from './useObserver';
 export { useLocalStorage } from './useLocalStorage';
-export { usePagination } from './usePagination';
 export { useFetchUsers } from './useFetchUsers';
